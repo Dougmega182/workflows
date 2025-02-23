d38d5cee67dd9934f89ab5076d7b8dcb7bc3faed
Dougmega182 <djbbinteractive@gmail.com>
2025-02-23 23:05:02 +0000

    modified:   backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use("/signin", signinRoutes);
 app.use("/signout", signoutRoutes);
 app.use("/reports", reportRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
