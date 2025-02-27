e1fcb27ba136e4706fdc04e0a655a2a012d97218
Dougmega182 <djbbinteractive@gmail.com>
2025-02-27 02:02:57 +0000

    renamed:    dist/static/index-6xnPfkFc.css -> build/assets/index-6xnPfkFc.css 	renamed:    dist/static/index-D_gCqhA0.js -> build/assets/index-P-7lumWQ.js 	new file:   build/index.html 	new file:   build/static/index-6xnPfkFc.css     new file:   build/static/index-D_gCqhA0.js 	modified:   vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: "./", // Ensures correct relative paths
   build: {
-    outDir: "dist",  // Use dist/ as output folder
-    assetsDir: "assets",  // Puts JS/CSS inside dist/assets
+    outDir: "dist",
+    assetsDir: "static",  // Moves assets to dist/static instead of dist/assets
   },
 });
